3d1b24c6bd6e2ec778ea5c7d052f9c3634b0795d
moon_racker <monster432max@live.com>
2020-02-26 23:10:59 +0300

    Init project

---
+++ src/main/java/com/bsuir/kononovich/InConnect/InConnectApplication.java
@@ -0,0 +1,13 @@
+package com.bsuir.kononovich.InConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bsuir/kononovich/InConnect/InConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bsuir.kononovich.InConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
