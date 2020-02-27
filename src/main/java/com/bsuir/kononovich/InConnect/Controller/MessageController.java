1dfccc5397045f72a5c40becb4cb4346e2172b1d
moon_racker <monster432max@live.com>
2020-02-28 00:05:59 +0300

    update

--- src/main/java/com/bsuir/kononovich/InConnect/Controller/MessageController.java
+++ src/main/java/com/bsuir/kononovich/InConnect/Controller/MessageController.java
@@ -1,10 +1,7 @@
 package com.bsuir.kononovich.InConnect.Controller;
 
 import com.bsuir.kononovich.InConnect.exceptions.NotFoundException;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +11,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("im")
 public class MessageController {
+    private int count = 4;
+
     public List<Map<String,String>> messages = new ArrayList<Map<String,String>>(){{
         add(new HashMap<String,String>() {{ put ("id", "1"); put("text", "test"); }});
         add(new HashMap<String,String>() {{ put ("id", "2"); put("text", "test2"); }});
@@ -25,10 +24,35 @@ public class MessageController {
     }
 
     @GetMapping("{id}")
-public Map<String,String> getOne(@PathVariable String id){
+    public Map<String,String> getOne(@PathVariable String id){
+        return getMessage(id);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
         return messages.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String > create(@RequestBody Map<String,String> message) {
+       message.put("id", String.valueOf(count++));
+       messages.add(message);
+       return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String,String> update(@PathVariable String id, @RequestBody Map<String,String> message) {
+        Map <String,String> messageFromDatabase = getMessage(message.get("id"));
+        messageFromDatabase.putAll(message);
+        messageFromDatabase.put("id", id);
+        return messageFromDatabase;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        Map<String,String> message = getMessage(id);
+        messages.remove(message);
     }
 }
