3ec37dfe531b802bd700c0991a7fa5e734d5fb92
moon_racker <monster432max@live.com>
2020-03-12 22:29:02 +0300

    added basic frontend for REST controller usin VueJS

--- src/main/java/com/bsuir/kononovich/InConnect/Controller/MessageController.java
+++ src/main/java/com/bsuir/kononovich/InConnect/Controller/MessageController.java
@@ -9,54 +9,54 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("im")
+@RequestMapping("/controllers/inconnect/messages")
 public class MessageController {
     private int count = 5;
 
-    public List<Map<String,String>> messages = new ArrayList<Map<String,String>>(){{
+    public List<Map<String,String>> msgDatabase = new ArrayList<Map<String,String>>(){{
         add(new HashMap<String, String>() {{
             put("id", "1");
-            put("text", "test");
+            put("text", "first string");
         }});
         add(new HashMap<String, String>() {{
             put("id", "2");
-            put("text", "test2");
+            put("text", "second string");
         }});
         add(new HashMap<String, String>() {{
             put("id", "3");
-            put("text", "test3");
+            put("text", "third string");
         }});
         add(new HashMap<String, String>() {{
             put("id", "4");
-            put("text", "test4");
+            put("text", "fourth string");
         }});
     }};
     @GetMapping
     public List<Map<String,String>> list() {
-        return messages;
+        return msgDatabase;
     }
 
     @GetMapping("{id}")
-    public Map<String,String> getOne(@PathVariable String id){
+    public Map<String,String> GetOneMessage(@PathVariable String id){
         return getMessage(id);
     }
 
     private Map<String, String> getMessage(@PathVariable String id) {
-        return messages.stream()
+        return msgDatabase.stream()
                 .filter(message -> message.get("id").equals(id))
                 .findFirst()
                 .orElseThrow(NotFoundException::new);
     }
 
     @PostMapping
-    public Map<String, String > create(@RequestBody Map<String,String> message) {
+    public Map<String, String > PostMessage(@RequestBody Map<String,String> message) {
        message.put("id", String.valueOf(count++));
-       messages.add(message);
+       msgDatabase.add(message);
        return message;
     }
 
     @PutMapping("{id}")
-    public Map<String,String> update(@PathVariable String id, @RequestBody Map<String,String> message) {
+    public Map<String,String> PutMessage(@PathVariable String id, @RequestBody Map<String,String> message) {
         Map <String,String> messageFromDatabase = getMessage(id);
         messageFromDatabase.putAll(message);
         messageFromDatabase.put("id", id);
@@ -64,8 +64,8 @@ public class MessageController {
     }
 
     @DeleteMapping("{id}")
-    public void delete(@PathVariable String id){
+    public void DeleteMessage(@PathVariable String id){
         Map<String,String> message = getMessage(id);
-        messages.remove(message);
+        msgDatabase.remove(message);
     }
 }
